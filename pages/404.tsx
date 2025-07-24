378ba2c047d348af79e51bbc1b4cd18664ae5052
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-07-24 10:54:13 +0530

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NotFound = () => (
+  <div>
+    <h1>PAGES ROUTER - 404 PAGE</h1>
+    <p>This page is using the PAGES ROUTER</p>
+  </div>
+)
+
+export default NotFound;
